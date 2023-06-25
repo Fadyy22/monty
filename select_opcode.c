d5135cd4239c1ce651c950da982a41e6d69735cc
Fadyy22 <fadyalaa441@gmail.com>
2023-06-25 13:48:27 +0300

    sub

--- select_opcode.c
+++ select_opcode.c
@@ -18,6 +18,7 @@ void (*select_opcode(char *code))(stack_t **stack, unsigned int line_number)
 		{"swap", swap},
 		{"add", add},
 		{"nop", nop},
+		{"sub", sub},
 		{NULL, NULL}};
 
 	for (i = 0; stack_functions[i].opcode; i++)
